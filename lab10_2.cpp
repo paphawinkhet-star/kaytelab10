7aec6f1b3b188b597e16a58c6d67a169b936bf03
paphawinkhet-star <paphawin.khet@gmail.com>
2025-12-25 13:38:08 +0700

    complete lab10_2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,8 +9,13 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string line;
+
+    dest << "-------------------- BOOM ---------------------" << endl;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+    dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
